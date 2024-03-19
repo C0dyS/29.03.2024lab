24d58b868aba3011c87e80a409729669daf75c5c
C0dyS <sashaculle@gmail.com>
2024-03-19 19:12:16 +0100

    4 task done

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-class Eg:
-    def __init__(self,amount):
-        self._amount = amount
+class Comp:
+    def __init__(self,processor,memory,gragh):
+        self._processor = processor
+        self._memory = memory
+        self._gragh = gragh
 
-    def check_balance(self):
-        return self._amount
+    def check_data(self):
+        return self._processor,self._memory,self._gragh,
+    def set_processor(self,new_processor):
+        self._processor = new_processor
+    def set_memory(self,new_memory):
+        self._memory = new_memory
+    def set_gragh(self,new_gragh):
+        self._gragh = new_gragh
 
-    def deposit(self,new_amount):
-        self._amount += new_amount
-
-    def withdraw(self,withdraw_amount):
-        if self._amount >= withdraw_amount:
-            self._amount -= withdraw_amount
-        else:
-            print('not enough $$$')
-
-
-test = Eg(100)
-print(test.check_balance())
-test.deposit(40)
-print(test.check_balance())
-test.withdraw(20)
-print(test.check_balance())
-test.withdraw(4000)
+my_comp = Comp('a','b','c')
+print(my_comp.check_data())
+my_comp.set_gragh('adf')
+print(my_comp.check_data())
